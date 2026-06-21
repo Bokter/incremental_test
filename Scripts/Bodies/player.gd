d59a2fab120158d5227e304bec6f8e3313218a1c
Bokter <rojasdjuan18@gmail.com>
2026-06-20 21:07:05 -0500

    Add weapon list and switching to player
    Expose and manage multiple weapons on the player: player.tscn now defines two gun instances (gun_1, gun_2) and a weapons array on the CharacterBody2D. player.gd adds an exported weapons array, current_weapon_index, max_weapons, and functions to switch weapons (mouse wheel), update active weapon visibility/processing, and add/replace weapons. Kept damage/fire-rate multipliers initialization. gun.gd was changed to extend Area2D (weapon nodes are areas) and retains exported bullet and barrels properties. Overall this enables multiple equipable weapons and runtime switching.

--- Scripts/Bodies/player.gd
+++ Scripts/Bodies/player.gd
@@ -1,20 +1,47 @@
 extends CharacterBody2D
-
 const SPEED = 300.0
-const BASE_SPEED = 300.0
-
-var augments: Array[Augment] = []
 
 @export var damage_multiplier: float = 1.0
 @export var fire_rate_multiplier: float = 1.0
+@export var weapons: Array[Area2D]
 
+var max_weapons = 2
+var current_weapon_index: int = 0
 
 func _ready():
 	Multiplier.set_damage_multiplier(damage_multiplier)
 	Multiplier.set_fire_rate_multiplier(fire_rate_multiplier)
+	update_active_weapon()
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			switch_weapon(1)
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			switch_weapon(-1)
+
+func switch_weapon(direction: int):
+	if weapons.is_empty():
+		return
+	current_weapon_index = wrapi(current_weapon_index + direction, 0, weapons.size())
+	update_active_weapon()
+
+func update_active_weapon():
+	for i in weapons.size():
+		var is_active = (i == current_weapon_index)
+		weapons[i].visible = is_active
+		weapons[i].set_process(is_active)
+
+func add_weapon(new_weapon: Area2D):
+	if weapons.size() < max_weapons:
+		weapons.append(new_weapon)
+	else:
+		weapons[current_weapon_index].queue_free()
+		weapons[current_weapon_index] = new_weapon
+	
+	update_active_weapon()
 
 func _physics_process(_delta):
-
 	var direction_x = Input.get_axis("left", "right")
 	var direction_y = Input.get_axis("up", "down")
 	
@@ -29,7 +56,3 @@ func _physics_process(_delta):
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 		
 	move_and_slide()
-
-
-func _on_enemy_area_entered(_area):
-	pass
--- Scripts/gun.gd
+++ Scripts/gun.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Area2D
 
 @export var bullet: PackedScene
 @export var barrels: Array[Node2D] 
